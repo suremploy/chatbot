24e40d9477e2d68b567c7f55a41068a994a9e6e4
justinlillico <149652474+justinlillico@users.noreply.github.com>
2024-12-20 01:48:53 +0000

    password added

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,47 +10,56 @@ st.write(
     "Fitness For Work Generator"
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-openai_api_key = st.secrets['OPENAI_KEY']
+auth = False
+if password := st.text_input("Enter the password", type="password"):
+    if password == st.secrets.get('PASSWORD', None):
+        auth = True
+    else:
+        st.error("Invalid password.")
 
-# Create an OpenAI client.
-client = openai.OpenAI(api_key=openai_api_key)
 
-# Create a session state variable to store the chat messages.
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+if auth:
+    # Ask user for their OpenAI API key via `st.text_input`.
+    openai_api_key = st.secrets['OPENAI_KEY']
 
-# Display the existing chat messages.
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Create an OpenAI client.
+    client = openai.OpenAI(api_key=openai_api_key)
 
-# Create a chat input field to allow the user to enter a message.
-if prompt := st.chat_input("Enter your prompt:"):
-    # Format the prompt using the INITIAL_PROMPT and RULES.
-    question_prompt = INITIAL_PROMPT.format(
-        assessment_details=TEST_PROMPT.replace("\n\n", "\n"),
-        gender="Female",
-        rules=RULES
-    )
+    # Create a session state variable to store the chat messages.
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-    # Store and display the user's input only.
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    # Display the existing chat messages.
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-    # Generate a response using the OpenAI API.
-    completion = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[{"role": "user", "content": question_prompt}],
-        temperature=0.0
-    )
+    # Create a chat input field to allow the user to enter a message.
+    if prompt := st.chat_input("Enter your prompt:"):
+        # Format the prompt using the INITIAL_PROMPT and RULES.
+        question_prompt = INITIAL_PROMPT.format(
+            assessment_details=TEST_PROMPT.replace("\n\n", "\n"),
+            gender="Female",
+            rules=RULES
+        )
 
-    # Get the response content.
-    response_content = completion.choices[0].message.content
-    response_content = response_content.replace("Ms X", f"Ms Smith")
+        # Store and display the user's input only.
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-    # Display and store the response.
-    with st.chat_message("assistant"):
-        st.markdown(response_content)
-    st.session_state.messages.append({"role": "assistant", "content": response_content})
+        # Generate a response using the OpenAI API.
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{"role": "user", "content": question_prompt}],
+            temperature=0.0
+        )
+
+        # Get the response content.
+        response_content = completion.choices[0].message.content
+        response_content = response_content.replace("Ms X", f"Ms Smith")
+
+        # Display and store the response.
+        with st.chat_message("assistant"):
+            st.markdown(response_content)
+        st.session_state.messages.append({"role": "assistant", "content": response_content})
